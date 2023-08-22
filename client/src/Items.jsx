636d68762cf35c835d7fa7dcbd360f342b99c39c
Anav Chand <anavkki@gmail.com>
2023-08-22 20:14:41 +1200

    :)

--- client/src/Items.jsx
+++ client/src/Items.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Button } from 'flowbite-react';
+import { Link } from 'react-router-dom'
 
 function Items() {
   const [items, setItems] = useState([
@@ -13,17 +14,22 @@ function Items() {
 
   return (
     <main className="grid h-screen w-screen place-content-center">
-        <div className="rounded-md p-2">
-          {items.map((item, index) => 
-            <div key={index} className="p-2 flex justify-between items-center gap-10">
-                <h1>{item.name}</h1>
-                <h1>{item.discepline}</h1>
-                <div className="flex items-center gap-5">
-                  <Button gradientDuoTone="greenToBlue" size="sm">Edit</Button>
-                  <Button gradientDuoTone="pinkToOrange" size="sm">Delete</Button>
-                </div>
-            </div> 
-          )}
+              <Link to="/create">
+                <Button gradientDuoTone="greenToBlue" size="sm">Create</Button>
+              </Link>
+      <div className="lift rounded-md p-2">
+        {items.map((item, index) =>
+          <div key={index} className="p-2 flex justify-between items-center gap-10">
+            <h1>{item.name}</h1>
+            <h1>{item.discepline}</h1>
+            <div className="flex items-center gap-5">
+              <Link to="/update">
+                <Button gradientDuoTone="greenToBlue" size="sm">Edit</Button>
+              </Link>
+              <Button gradientDuoTone="pinkToOrange" size="sm">Edit</Button>
+            </div>
+          </div>
+        )}
       </div>
     </main>
   )
--- client/src/UpdateItem.jsx
+++ client/src/UpdateItem.jsx
@@ -1,8 +1,40 @@
+import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+
 function UpdateItem() {
     return (
-     <div>
-          <h1>UpdateItem</h1>
-     </div>
+     <main className="grid h-screen w-screen place-content-center">
+           <form className="flex max-w-md flex-col gap-4 w-96">
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="name"
+            value="Your Name"
+          />
+        </div>
+        <TextInput
+          id="name"
+          required
+          type="text"
+        />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="discepline"
+            value="Your Discepline"
+          />
+        </div>
+        <TextInput
+          id="discepline"
+          required
+          type="text"
+        />
+      </div>
+      <Button type="submit" gradientDuoTone="greenToBlue">
+        Submit
+      </Button>
+    </form>
+     </main>
     )
   }
   
